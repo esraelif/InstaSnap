c111b6ec37c7c5e1a36d4b18405cf7e3ff3f4d08
esraelif <elifesratunca@gmail.com>
2024-03-15 15:11:04 +0100

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,33 @@
+import { Routes,Route } from 'react-router-dom';
+import './global.css'
+import SigninForm from './_auth/forms/SigninForm';
+import { Home } from './_root/pages';
+import AuthLayout from './_auth/AuthLayout';
+import SignupForm from './_auth/forms/SignupForm';
+import RootLayout from './_root/RootLayout';
+
+const App = () => {
+  return (
+  <main className='flex h-screen'>
+    <Routes>
+        {/* public routes */}
+        <Route element={<AuthLayout/>}>
+             <Route  path='/sign-in' element={<SigninForm/>}/>
+             <Route  path='/sign-up' element={<SignupForm/>}/>
+        </Route>
+
+
+        {/* public routes */}
+        <Route element={<RootLayout/>}>
+            <Route index element={<Home/>}/>
+        </Route>
+       
+
+
+    </Routes>
+
+  </main>
+  );
+}
+
+export default App;
---
+++ src/_root/RootLayout.tsx
@@ -0,0 +1,11 @@
+
+
+const RootLayout = () => {
+  return (
+    <div>
+    Rootlayout
+    </div>
+  );
+}
+
+export default RootLayout;
